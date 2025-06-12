2ac90929fa00f6a7f6f786ea3c6417d50de302c9
connieha3436 <connh99@gmail.com>
2025-06-11 21:03:03 -0400

    home page with JS, text only for about me page, and working on resume page

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,8 @@
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 //..
-AOS.init();
+AOS.init({
+    duration: 1000,
+    once: true});
 
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
